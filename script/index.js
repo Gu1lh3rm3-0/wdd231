da6387fdb020d5e5083d56b41ba7adf99dd5614e
Gu1lh3rm3-0 <guilherme.oliver2002@gmail.com>
2026-01-16 17:22:02 -0300

    fixed

--- script/index.js
+++ script/index.js
@@ -1,9 +1,1 @@
-const menu = document.querySelector("#menu");
-const nav = document.querySelector(".navigation");
-
-menu.addEventListener("click", () => {
-    nav.classList.toggle("show");
-    menu.classList.toggle("show");
-})
-
 document.getElementById("lastModified").innerHTML = document.lastModified;
