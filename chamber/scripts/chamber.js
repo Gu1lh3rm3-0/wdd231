9b2564210873da3936971ba5b26e1067c997d255
Gu1lh3rm3-0 <guilherme.oliver2002@gmail.com>
2026-03-14 18:52:06 -0300

    Week 1

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,1 +1,38 @@
-const data =
+const cards = document.querySelector('#cards');
+
+async function getCompaniesData() {
+    try {
+        const response = await fetch('chamber/data/companies.json');
+        const data = await response.json();
+        displayCompanies(data.companies);
+    } catch (error) {
+        console.error('Error fetching companies data:', error);
+    }
+}
+
+getCompaniesData();
+
+const displayCompanies = (companies) => {
+    companies.forEach((company) => {
+        let card = document.createElement('section');
+        let fullname = document.createElement('h2');
+        let logo = document.createElement('img');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a');
+        
+        logo.setAttribute('src', company.logo);
+        logo.setAttribute('alt', `Logo of ${company.name}`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '200');
+        logo.setAttribute('height', '200');
+
+        card.appendChild(logo);
+        card.appendChild(fullname);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        cards.appendChild(card);
+
+    });
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,33 @@
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const courses = [
+    { name: "WDD 130", category: "wdd", credit: 2 },
+    { name: "WDD 131", category: "wdd", credit: 2 },
+    { name: "WDD 231", category: "wdd", credit: 2 },
+    { name: "CSE 210", category: "cse", credit: 2 },
+    { name: "CSE 212", category: "cse", credit: 2 }
+];
+
+function filter(category) {
+    let filteredCourses;
+
+    if (category === "all") {
+        filteredCourses = courses;
+    } else {
+        filteredCourses = courses.filter(course => course.category === category);
+    }
+
+    showCourses(filteredCourses);
+}
+
+function showCourses(courseList) {
+    let html = "";
+    let totalCredits = 0;
+
+    courseList.forEach(course => {
+        html += `<div class="item">${course.name}</div>`;
+        totalCredits += course.credit;
+    });
+    document.querySelector(".course-list").innerHTML = html;
+    document.querySelector(".course-credit").innerHTML = `The total credits listed for the course listed above is: ${totalCredits}`;
+}
