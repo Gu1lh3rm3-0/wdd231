90bfed4c8bb6b5e548a1bb8eaee695310b35f59b
Gu1lh3rm3-0 <guilherme.oliver2002@gmail.com>
2026-03-21 20:03:04 -0300

    Last

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -67,6 +67,5 @@ btn.addEventListener("click", () => {
   btn.classList.toggle("active");
   menu.classList.toggle("active");
 });
-
 document.getElementById("lastModified").innerHTML = document.lastModified;
 document.getElementById("currentyear").textContent = new Date().getFullYear();
