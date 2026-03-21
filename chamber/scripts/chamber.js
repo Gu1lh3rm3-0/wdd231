1bbcf20ffef87a91d0e76b18df8f92fb68bde922
Gu1lh3rm3-0 <guilherme.oliver2002@gmail.com>
2026-03-21 17:31:02 -0300

    Almost finishing

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,38 +1,47 @@
-const cards = document.querySelector('#cards');
+const cards = document.querySelector('.cards');
+const members = 'https://gu1lh3rm3-0.github.io/wdd231/chamber/data/members.json';
 
 async function getCompaniesData() {
-    try {
-        const response = await fetch('chamber/data/companies.json');
-        const data = await response.json();
-        displayCompanies(data.companies);
-    } catch (error) {
-        console.error('Error fetching companies data:', error);
-    }
+    const response = await fetch(members);
+    const data = await response.json();
+    displayCompanies(data.companies);
 }
 
 getCompaniesData();
 
 const displayCompanies = (companies) => {
     companies.forEach((company) => {
-        let card = document.createElement('section');
-        let fullname = document.createElement('h2');
-        let logo = document.createElement('img');
-        let address = document.createElement('p');
-        let phone = document.createElement('p');
-        let website = document.createElement('a');
-        
-        logo.setAttribute('src', company.logo);
-        logo.setAttribute('alt', `Logo of ${company.name}`);
-        logo.setAttribute('loading', 'lazy');
-        logo.setAttribute('width', '200');
-        logo.setAttribute('height', '200');
+        let card = document.createElement("section");
+        let name = document.createElement("h2");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let url = document.createElement("a");
+        let email = document.createElement("a");
+        let membership = document.createElement("a");
+        let image = document.createElement("img");
 
-        card.appendChild(logo);
-        card.appendChild(fullname);
+        name.textContent = company.name;
+        address.textContent = company.address;
+        phone.textContent = company.phone;
+        url.textContent = company.url;
+        url.setAttribute("href", company.url);
+        email.textContent = company.email;
+        email.setAttribute("href", `E-mail:${company.email}`);
+        membership.textContent = company.membership;
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '200');
+        image.setAttribute("src", company.logo);
+        image.setAttribute('alt', `Logo of ${company.name}`);
+
+        card.appendChild(image);
+        card.appendChild(name);
         card.appendChild(address);
         card.appendChild(phone);
-        card.appendChild(website);
+        card.appendChild(url);
+        card.appendChild(email);
+        card.appendChild(membership);
         cards.appendChild(card);
-
     });
 }
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
