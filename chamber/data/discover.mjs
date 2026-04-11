6a21490619d061968e7e57d2333f2c20d0bc5230
Gu1lh3rm3-0 <guilherme.oliver2002@gmail.com>
2026-04-11 10:15:03 -0300

    images

---
+++ chamber/data/discover.mjs
@@ -0,0 +1,50 @@
+export const discover =[
+  {
+    "name": "Avenida Paulista",
+    "address": "Avenida Paulista – São Paulo, SP, Brazil",
+    "description": "The city’s main avenue, packed with museums, shops, and cultural life. On Sundays, it becomes a pedestrian street full of music and events.",
+    "photo_url": "avenida-paulista.webp"
+  },
+  {
+    "name": "Parque Ibirapuera",
+    "address": "Av. Pedro Álvares Cabral – Vila Mariana, São Paulo – SP, Brazil",
+    "description": "One of the largest urban parks in Brazil, perfect for relaxing, exercising, and visiting cultural spaces.",
+    "photo_url": ""
+  },
+  {
+    "name": "Museu de Arte de São Paulo (MASP)",
+    "address": "Av. Paulista, 1578 – Bela Vista, São Paulo – SP, 01310-200, Brazil",
+    "description": "A world-famous museum known for its floating glass design and important art collection.",
+    "photo_url": ""
+  },
+  {
+    "name": "Edifício Copan",
+    "address": "Av. Ipiranga, 200 – República, São Paulo – SP, Brazil",
+    "description": "An iconic curved building designed by Oscar Niemeyer, offering great city views and a lively atmosphere.",
+    "photo_url": ""
+  },
+  {
+    "name": "Mercado Municipal de São Paulo",
+    "address": "R. da Cantareira, 306 – Centro Histórico, São Paulo – SP, 01024-900, Brazil",
+    "description": "A historic market famous for its gourmet food, fruits, and the iconic mortadella sandwich.",
+    "photo_url": ""
+  },
+  {
+    "name": "Theatro Municipal de São Paulo",
+    "address": "Praça Ramos de Azevedo, s/n – República, São Paulo – SP, Brazil",
+    "description": "A stunning theater inspired by European architecture, hosting concerts, opera, and tours.",
+    "photo_url": ""
+  },
+  {
+    "name": "Beco do Batman",
+    "address": "Rua Gonçalo Afonso – Vila Madalena, São Paulo – SP, Brazil",
+    "description": "A colorful alley full of graffiti art that changes constantly—perfect for photos.",
+    "photo_url": ""
+  },
+  {
+    "name": "Vila Madalena",
+    "address": "Vila Madalena – São Paulo, SP, Brazil",
+    "description": "A bohemian neighborhood known for nightlife, bars, live music, and an artistic vibe.",
+    "photo_url": ""
+  }
+]
